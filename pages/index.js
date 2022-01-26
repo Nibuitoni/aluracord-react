79ddd2f89e3079bfbc2c0d2ab01fc3f118e258ae
Nicole Buitoni <nicole.buitoni@rdstation.com>
2022-01-25 23:21:59 -0300

    class 2

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json'
-
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-  }
 
 function Title(props) {
     const Tag = props.tag || 'h1';
@@ -60,11 +33,12 @@ function Title(props) {
 //   export default HomePage
 
   export default function PaginaInicial() {
-    const username = 'Nibuitoni';
-  
+    // const username = 'Nibuitoni';
+    const [username, setUsername] = React.useState('Nibuitoni');
+    const routing = useRouter();
+    
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -91,6 +65,11 @@ function Title(props) {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={ function (eventInfos) {
+                eventInfos.preventDefault();
+                // console.log('Someone submited the form')
+                routing.push('/chat');
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -100,8 +79,15 @@ function Title(props) {
               <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                 {appConfig.name}
               </Text>
-  
               <TextField
+               value={username}
+               onChange={function (event) {
+                 console.log('usuario digitou', event.target.value);
+                 // Where is the value?
+                 const value = event.target.value;
+                 // Change the variable value
+                 setUsername(value);
+               }}
                 fullWidth
                 textFieldColors={{
                   neutral: {
